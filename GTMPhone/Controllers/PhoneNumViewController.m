3e8638f4a47ccaec5f6b00bbda4c0c088857f8b6
Yuyi <yuyi@geetest.com>
2018-03-02 17:10:26 +0800

    Documents Updated.

--- GTMPhone/Controllers/PhoneNumViewController.m
+++ GTMPhone/Controllers/PhoneNumViewController.m
@@ -6,12 +6,21 @@
 //  Copyright © 2017 geetest. All rights reserved.
 //
 
+/**
+* 开发者须读：
+* 极验提供 `身份验证`（极验 OnePass）和 `人机验证` (Sensebot), 开发者在部署验证过程中，请提前阅读以下几点：
+* - 可单独部署 `身份验证`, 也可结合部署 `人机验证` 和 `身份验证`;
+* - 部署上述两种验证模式均需联系极验后台修改相应的配置;
+* - 本 Demo 提供两个常见场景模拟，分别是
+*    - 登录场景：演示部署 `身份验证`
+*    - 注册场景：演示部署 `人机验证` 和 `身份验证`
+*/
+
 #import "PhoneNumViewController.h"
 #import "VerifySMSViewController.h"
 #import "ResultViewController.h"
 
 #import "TipsView.h"
-
 #import "GT3CaptchaEX.h"
 
 #import "UIButton+GTM.h"
@@ -19,16 +28,19 @@
 
 @import GTOnePass;
 
-//网站主部署的用于test-Button的register接口
+
+// 网站主部署的用于test-Button的register接口
 #define API1 @"http://www.geetest.com/demo/gt/register-test"
-//网站主部署的用于test-Button的validate接口
+// 网站主部署的用于test-Button的validate接口
 #define API2 @"http://www.geetest.com/demo/gt/validate-test"
 
-//网站主部署的ONEPASS的校验接口
+// 网站主部署的ONEPASS的校验接口
 #define verify_url @"http://onepass.geetest.com/check_gateway.php"
 
 @interface PhoneNumViewController () <UITextFieldDelegate, SMSCodeDelegate, ResultVCDelegate, GOPManagerDelegate, GT3CaptchaManagerDelegate>
 
+
+#pragma mark - Properties
 @property (weak, nonatomic) IBOutlet UITextField *phoneNumTextField;
 @property (weak, nonatomic) IBOutlet UILabel *titleLabel;
 @property (weak, nonatomic) IBOutlet UIButton *nextButton;
@@ -44,47 +56,40 @@
 
 @implementation PhoneNumViewController
 
-- (IBAction)back:(id)sender {
-    if ([self.phoneNumTextField canResignFirstResponder]) {
-        [self.phoneNumTextField resignFirstResponder];
-    }
-    
-    [self dismissViewControllerAnimated:YES completion:nil];
-}
 
-- (IBAction)nextStep:(id)sender {
-    [self verifyPhoneNum];
-    if ([self.phoneNumTextField canResignFirstResponder]) {
-        [self.phoneNumTextField resignFirstResponder];
-    }
-}
-
+#pragma mark - Getter methods
 - (GOPManager *)manager {
     if (!_manager) {
-        _manager = [[GOPManager alloc] initWithCustomID:@"---<Your Custom ID>---" verifyUrl:verify_url timeout:10.0];
+        
+        NSString *customID = nil;
+        if ([self.type isEqualToString:@"login"]) { // 在登录场景下展示 `身份验证`
+            customID = @"请使用在极验后台申请的customID, 用于`身份验证`. ";
+            
+        } else if ([self.type isEqualToString:@"register"]) { // 在注册场景下展示 `人机验证` 和 `身份验证`
+            customID = @"请使用在极验后台申请的customID, 用于`人机验证` 和 `身份验证`. ";
+        }
+        
+        _manager = [[GOPManager alloc] initWithCustomID:customID verifyUrl:verify_url timeout:10.0];
+        _manager.phoneNumEncryptOption = GOPPhoneNumEncryptOptionNone;
         _manager.delegate = self;
     }
     
     return _manager;
 }
 
-- (void)dealloc {
-    [self.captchaEx stopGT3Captcha];
-}
-
+#pragma mark - Life Cycle
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    self.captchaEx = [[GT3CaptchaEX alloc] initWithApi1:API1 api2:API2 timeout:1.0];
-    self.captchaEx.manager.delegate = self;
-    [self.captchaEx registerGT3Captcha];
-    
     if ([self.type isEqualToString:@"login"]) {
         self.titleLabel.text = @"请登录";
-    }
-    
-    if ([self.type isEqualToString:@"register"]) {
+        
+    } else if ([self.type isEqualToString:@"register"]) {
         self.titleLabel.text = @"请注册";
+
+        self.captchaEx = [[GT3CaptchaEX alloc] initWithApi1:API1 api2:API2 timeout:4.0];
+        self.captchaEx.manager.delegate = self;
+        [self.captchaEx registerGT3Captcha];
     }
     
     if ([self.phoneNumTextField canBecomeFirstResponder]) {
@@ -99,49 +104,62 @@
     [self.nextButton gtm_removeIndicator];
 }
 
-// 校验手机号格式及国别
+- (void)dealloc {
+    [self.captchaEx stopGT3Captcha];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+#pragma mark - Prepare Captcha
+
 - (void)verifyPhoneNum {
     
     NSString *num = self.phoneNumTextField.text;
     
-    if (![self checkPhoneNumFormat:num]) {
+    if (![self checkPhoneNumFormat:num]) { // 校验手机号格式及国别
         self.phoneNumTextField.text = nil;
         [self.phoneNumTextField gtm_shake:9 witheDelta:2.f speed:0.1 completion:nil];
         [TipsView showTipOnKeyWindow:@"不合法的手机号"];
         return;
     }
-    
-    [self startCaptcha];
-}
-
-// 进行人机验证
-- (void)startCaptcha {
     [self.nextButton gtm_showIndicator];
-    [self.captchaEx startGT3Captcha];
+    
+    if ([self.type isEqualToString:@"login"]) {
+        [self startOnePass:nil]; // Note: 仅使用`身份验证`时，validate 置为 nil.
+        
+    } else if ([self.type isEqualToString:@"register"]) {
+        [self.captchaEx startGT3Captcha]; // Note: 使用 `身份验证` 和 `人机验证` 时，需从 `人机验证` 成功后取得 validate 供 `身份验证` 流程使用.
+    }
 }
 
-// 获取人机验证的validate后, 进行onepass手机号码校验
+// 进行 OnePass 手机号码校验
 - (void)startOnePass:(NSString *)validate {
     NSString *num = self.phoneNumTextField.text;
     
-    if (![self checkPhoneNumFormat:num]) {// check phone num, country code
+    if (![self checkPhoneNumFormat:num]) { // check phone num, country code
         self.phoneNumTextField.text = nil;
         [self.phoneNumTextField gtm_shake:9 witheDelta:2.f speed:0.1 completion:nil];
         [TipsView showTipOnKeyWindow:@"不合法的手机号"];
         return;
     }
     
-    if (!self.manager.diagnosisStatus) {// check onepass network
+    if (!self.manager.diagnosisStatus) { // check OnePass network
         [TipsView showTipOnKeyWindow:@"OnePass需要您的数据网络支持。如果确认已开启数据网络, 可能是您当前的网络不被支持。"];
         return;
     }
-    
+    [self.manager verifyPhoneNum:@"" withCaptchaValidate:nil completion:^(NSDictionary *dict) {
+        
+    } failure:^(NSError *error) {
+        
+    }];
     [self.manager verifyPhoneNum:num withCaptchaValidate:validate completion:^(NSDictionary *dict) {
         NSLog(@"completion: %@", dict.description);
         
         NSString *type = [dict objectForKey:@"type"];
         NSNumber *result = [dict objectForKey:@"result"];
-        if ([type isEqualToString:@"onepass"] && [result isEqualToNumber:@(0)]) {// onepass成功, 输入的手机号码和本机的sim一致
+        if ([type isEqualToString:@"onepass"] && [result isEqualToNumber:@(0)]) { // onepass成功, 输入的手机号码和本机的sim一致
             
             [self.nextButton gtm_removeIndicator];
             
@@ -153,11 +171,11 @@
                 [self presentViewController:vc animated:YES completion:nil];
             });
         }
-        else if ([type isEqualToString:@"onepass"] && ![result isEqualToNumber:@(0)]) {// onepass失败且没有购买极验的短信验证服务。一般后续处理为, 调用自己的短信验证码服务。
+        else if ([type isEqualToString:@"onepass"] && ![result isEqualToNumber:@(0)]) { // onepass失败且没有购买极验的短信验证服务。一般后续处理为, 调用自己的短信验证码服务。
             
             [TipsView showTipOnKeyWindow:@"OnePass未通过" fontSize:14.0];
         }
-        else if ([type isEqualToString:@"sms"]) {//  onepass失败且购买了极验的短信服务, 极验短信验证码已发送成功后收到此结果。下面需要创建UI来处理用户输入的短信验证码。c
+        else if ([type isEqualToString:@"sms"]) { // onepass失败且购买了极验的短信服务, 极验短信验证码已发送成功后收到此结果。下面需要创建UI来处理用户输入的短信验证码。
             
             NSString *desc = [NSString stringWithFormat:@"短信原因:\n%@", dict.description];
             [TipsView showTipOnKeyWindow:desc fontSize:14.0];
@@ -187,12 +205,136 @@
         
     } failure:^(NSError *error) {
         NSString *desc = [NSString stringWithFormat:@"错误信息:\n%@", error.description];
-        if (error.code != -999) [TipsView showTipOnKeyWindow:desc fontSize:14.0];// ignore -999
-        //发生了错误
+        if (error.code != -999) [TipsView showTipOnKeyWindow:desc fontSize:14.0]; // ignore -999
+        // 发生了错误
         NSLog(@"error: %@", error);
         [self.nextButton gtm_removeIndicator];
     }];
 }
+
+#pragma mark - GOPManagerDelegate 身份验证代理方法
+
+// Customize the request for VERIFY api and you can modify properties of NSURLRequest's instance such as HTTPMethod, timeoutInterval, allHTTPHeaderFields and so on
+/*
+ - (void)gtOnePass:(GOPManager *)manager willRequestVerify:(NSURLRequest *)originalRequest withReplacedHandler:(void (^)(NSURLRequest *))replacedHandler {
+ NSMutableURLRequest *mRequest = [originalRequest mutableCopy];
+ 
+ NSString *originalAbsoluteURLString = mRequest.URL.absoluteURL.absoluteString;
+ NSString *newString = [NSString stringWithFormat:@"%@%@", originalAbsoluteURLString, @"&testToken=test"];
+ NSURL *url = [NSURL URLWithString:newString];
+ mRequest.URL = url;
+ mRequest.HTTPMethod = @"POST";
+ mRequest.timeoutInterval = 5.0;
+ replacedHandler([mRequest copy]);
+ }
+ */
+
+
+#pragma mark - GT3CaptchaManager Delegate 人机验证代理方法
+
+- (void)gtCaptchaUserDidCloseGTView:(GT3CaptchaManager *)manager {
+    [self.nextButton gtm_removeIndicator];
+    [TipsView showTipOnKeyWindow:@"用户取消了人机验证"];
+}
+
+- (void)gtCaptcha:(GT3CaptchaManager *)manager errorHandler:(GT3Error *)error {
+    [self.nextButton gtm_removeIndicator];
+    [TipsView showTipOnKeyWindow:error.description];
+}
+
+// disable secondary validate when using OnePass
+- (BOOL)shouldUseDefaultSecondaryValidate:(GT3CaptchaManager *)manager {
+    return NO;
+}
+
+- (void)gtCaptcha:(GT3CaptchaManager *)manager didReceiveSecondaryCaptchaData:(NSData *)data response:(NSURLResponse *)response error:(GT3Error *)error decisionHandler:(void (^)(GT3SecondaryCaptchaPolicy))decisionHandler {
+    // If `shouldUseDefaultSecondaryValidate:` return NO, do nothing here
+    
+}
+
+// put captcha result into OnePass
+- (void)gtCaptcha:(GT3CaptchaManager *)manager didReceiveCaptchaCode:(NSString *)code result:(NSDictionary *)result message:(NSString *)message {
+    
+    if (![code isEqualToString:@"1"]) return;
+    
+    NSString *validate = [result objectForKey:@"geetest_validate"];
+    
+    if (!validate || validate.length != 32) return;
+    
+    [self startOnePass:validate];
+}
+
+- (void)gtCaptcha:(GT3CaptchaManager *)manager willSendRequestAPI1:(NSURLRequest *)originalRequest withReplacedHandler:(void (^)(NSURLRequest *))replacedHandler {
+    // add timestamp to api1
+    NSMutableURLRequest *mRequest = [originalRequest mutableCopy];
+    
+    NSURLComponents *comp = [[NSURLComponents alloc] initWithString:mRequest.URL.absoluteString];
+    NSMutableArray *items = [comp.queryItems mutableCopy];
+    [items enumerateObjectsUsingBlock:^(NSURLQueryItem * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+        NSURLQueryItem *item = (NSURLQueryItem *)obj;
+        if (item.name && [item.name isEqualToString:@"t"]) {
+            NSString *time = [NSString stringWithFormat:@"%.0f", [[NSDate date] timeIntervalSince1970] * 1000];
+            [items removeObject:obj];
+            NSURLQueryItem *new = [[NSURLQueryItem alloc] initWithName:@"t" value:time];
+            [items addObject:new];
+        }
+    }];
+    comp.queryItems = items;
+    mRequest.URL = comp.URL;
+    
+    replacedHandler(mRequest);
+}
+
+#pragma mark - DEMO Delegate
+
+- (void)smsVCDidSuccess:(NSDictionary *)dict {
+    
+    [self.nextButton gtm_removeIndicator];
+    
+    UIStoryboard *sb = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
+    ResultViewController *vc = [sb instantiateViewControllerWithIdentifier:@"result"];
+    vc.delegate = self;
+    vc.type = self.type;
+    [self presentViewController:vc animated:YES completion:nil];
+}
+
+- (void)smsVCWillResend {
+    [self verifyPhoneNum];
+}
+
+- (void)resultVCDidReturn {
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
+    if (textField.text.length == 11) {
+        [self verifyPhoneNum];
+        if ([textField canResignFirstResponder]) {
+            [textField resignFirstResponder];
+        }
+        return YES;
+    }
+    else return NO;
+}
+
+#pragma mark - IBAction
+
+- (IBAction)back:(id)sender {
+    if ([self.phoneNumTextField canResignFirstResponder]) {
+        [self.phoneNumTextField resignFirstResponder];
+    }
+    
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (IBAction)nextStep:(id)sender {
+    [self verifyPhoneNum];
+    if ([self.phoneNumTextField canResignFirstResponder]) {
+        [self.phoneNumTextField resignFirstResponder];
+    }
+}
+
+#pragma mark - Helper methods
 
 // 检测手机号码格式的参考示范
 - (BOOL)checkPhoneNumFormat:(NSString *)num {
@@ -255,96 +397,5 @@
     else return NO;
 }
 
-#pragma mark GT3CaptchaManager
-
-- (void)gtCaptchaUserDidCloseGTView:(GT3CaptchaManager *)manager {
-    [self.nextButton gtm_removeIndicator];
-    [TipsView showTipOnKeyWindow:@"用户取消了人机验证"];
-}
-
-- (void)gtCaptcha:(GT3CaptchaManager *)manager errorHandler:(GT3Error *)error {
-    [self.nextButton gtm_removeIndicator];
-    [TipsView showTipOnKeyWindow:error.description];
-}
-
-// disable secondary validate when using OnePass
-- (BOOL)shouldUseDefaultSecondaryValidate:(GT3CaptchaManager *)manager {
-    return NO;
-}
-
-- (void)gtCaptcha:(GT3CaptchaManager *)manager didReceiveSecondaryCaptchaData:(NSData *)data response:(NSURLResponse *)response error:(GT3Error *)error decisionHandler:(void (^)(GT3SecondaryCaptchaPolicy))decisionHandler {
-    // If `shouldUseDefaultSecondaryValidate:` return NO, do nothing here
-    
-}
-
-// put captcha result into OnePass
-- (void)gtCaptcha:(GT3CaptchaManager *)manager didReceiveCaptchaCode:(NSString *)code result:(NSDictionary *)result message:(NSString *)message {
-    
-    if (![code isEqualToString:@"1"]) return;
-    
-    NSString *validate = [result objectForKey:@"geetest_validate"];
-    
-    if (!validate || validate.length != 32) return;
-    
-    [self startOnePass:validate];
-}
-
-- (void)gtCaptcha:(GT3CaptchaManager *)manager willSendRequestAPI1:(NSURLRequest *)originalRequest withReplacedHandler:(void (^)(NSURLRequest *))replacedHandler {
-    // add timestamp to api1
-    NSMutableURLRequest *mRequest = [originalRequest mutableCopy];
-    
-    NSURLComponents *comp = [[NSURLComponents alloc] initWithString:mRequest.URL.absoluteString];
-    NSMutableArray *items = [comp.queryItems mutableCopy];
-    [items enumerateObjectsUsingBlock:^(NSURLQueryItem * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
-        NSURLQueryItem *item = (NSURLQueryItem *)obj;
-        if (item.name && [item.name isEqualToString:@"t"]) {
-            NSString *time = [NSString stringWithFormat:@"%.0f", [[NSDate date] timeIntervalSince1970] * 1000];
-            [items removeObject:obj];
-            NSURLQueryItem *new = [[NSURLQueryItem alloc] initWithName:@"t" value:time];
-            [items addObject:new];
-        }
-    }];
-    comp.queryItems = items;
-    mRequest.URL = comp.URL;
-    
-    replacedHandler(mRequest);
-}
-
-#pragma mark DEMO Delegate
-
-- (void)smsVCDidSuccess:(NSDictionary *)dict {
-    
-    [self.nextButton gtm_removeIndicator];
-    
-    UIStoryboard *sb = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
-    ResultViewController *vc = [sb instantiateViewControllerWithIdentifier:@"result"];
-    vc.delegate = self;
-    vc.type = self.type;
-    [self presentViewController:vc animated:YES completion:nil];
-}
-
-- (void)smsVCWillResend {
-    [self verifyPhoneNum];
-}
-
-- (void)resultVCDidReturn {
-    [self dismissViewControllerAnimated:YES completion:nil];
-}
-
-- (BOOL)textFieldShouldReturn:(UITextField *)textField {
-    if (textField.text.length == 11) {
-        [self verifyPhoneNum];
-        if ([textField canResignFirstResponder]) {
-            [textField resignFirstResponder];
-        }
-        return YES;
-    }
-    else return NO;
-}
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
 
 @end
--- README.md
+++ README.md
@@ -2,9 +2,9 @@
 
 了解产品: [www.geetest.com](www.geetest.com)
 
-需要拖入仓库`SDK`路径下的`GTOnePass.framework`, `TYRENoUISDK.framework` 4个文件
+需要拖入仓库`SDK`路径下的`GTOnePass.framework`, `TYRENoUISDK.framework` 2个文件
 
-同时需要集成`test-Button`产品的`GT3Captcha.framework`, `GT3Captcha.bundle`文件以获取相关参数。
+同时需要集成`Sensebot`产品的`GT3Captcha.framework`, `GT3Captcha.bundle`文件以获取相关参数。
 
 # 概述及资源
 
@@ -12,18 +12,18 @@
 
 条目	|资源 			
 ------	|------------	
-开发目标|兼容iOS7, 推荐iOS8+		
+开发目标|兼容 iOS7, 推荐 iOS8+		
 开发环境|Xcode 8.0	
 系统依赖|`Webkit.framework`, `JavascriptCore.framework`
-产品依赖|[test-Button](http://docs.geetest.com/install/overview/)
-sdk三方依赖|无		
+产品依赖|[Sensebot](http://docs.geetest.com/install/overview/)
+SDK 三方依赖|无		
 
 ## 相关开发资料
 
 条目|资源|
 -------------	|--------------
-SDK下载			|[gop-ios-sdk](http://github.com/GeeTeam/gop-ios-sdk)
-接口文档		|[gop-ios-header-docs](http://github.com/GeeTeam/gop-ios-sdk/blob/master/SDK/gop-ios-dev-doc.md)或查看头文件注释
+SDK 下载			|[gop-ios-sdk](http://github.com/GeeTeam/gop-ios-sdk)
+接口文档		|[gop-ios-header-docs](http://github.com/GeeTeam/gop-ios-sdk/blob/master/SDK/gop-ios-dev-doc.md) 或查看头文件注释
 错误码			|[Error Code](http://github.com/GeeTeam/gop-ios-sdk/blob/master/SDK/gop-ios-dev-doc.md#errorcode)
 产品结构流程  	|[交互流程](http://docs.geetest.com/onepass/overview/#交互流程),[通讯流程](http://docs.geetest.com/onepass/overview/#通讯流程)
 
@@ -44,34 +44,43 @@ git clone git@github.com:GeeTeam/gop-ios-sdk.git
 
 ## 导入SDK并配置环境
 
-1. 如果您是手动添加SDK, 将下载获取的`GTOnePass.framework`, `TYRZNoUISDK.framework`,`GT3Captcha.framework`及`GT3Captcha.bunele`4个文件拖拽到工程中, 确保`Copy items if needed`已被勾选。
+1. 将下载获取的`GTOnePass.framework`, `TYRZNoUISDK.framework`,`GT3Captcha.framework`及`GT3Captcha.bunele`4个文件拖拽到工程中, 确保`Copy items if needed`已被勾选。
 
 	![import](./img/import.png)
 	
-	请使用`Linked Frameworks and Libraries`方式导入framework。在拖入`GTOnePass.framework`,`TYRZNoUISDK.framework`和`GT3Captcha.framework`到工程时后, 请检查`.framework`是否被添加到`PROJECT -> Build Phases -> Linked Frameworks and Libraries`, 以确保正常编译。
+	请使用`Linked Frameworks and Libraries`方式导入 framework。在拖入`GTOnePass.framework`,`TYRZNoUISDK.framework`和`GT3Captcha.framework`到工程后, 请检查`.framework`是否被添加到`PROJECT -> Build Phases -> Linked Frameworks and Libraries`, 以确保正常编译。
 	
 	![linkedlibraries](./img/linkedlibraries.png)
 
-2. 针对静态库中的`Category`, 需要在对应target的`Build Settings`->`Other Linker Flags`添加`-ObjC`编译选项。如果依然有问题，再添加`-all_load`。
+2. 针对静态库中的`Category`, 需要在对应 target 的`Build Settings`->`Other Linker Flags`添加`-ObjC`编译选项。如果依然有问题，再添加`-all_load`。
 
 	![linkerflags](./img/linkerflags.png)
 
 ## 配置接口
 
-开发者集成客户端sdk前, 必须先在您的服务器上搭建相应的**服务端SDK**，并配置从[极验后台](https://account.geetest.com/login)获取的`id`和`key`, 并且将配置的用户获取配置的接口`config_url`和`verify_url`放入客户端的初始化方法中。
+开发者集成客户端 SDK 前, 必须先在您的服务器上搭建相应的**服务端 SDK**，配置`verify_Url`, 并配置从[极验后台](https://account.geetest.com/login)获取的`customID`。这里以服务端 `verify_Url` 配置成功，客户端开发步骤为例，如下：
 
-集成用户需要使用iOS SDK完成提供的以下接口:
+1. 用 `verify_Url` 和 `customID` 初始化 GOPManager 实例
 
-1. 配置并初始化
+	```Objective-C
+	GOPManager *manager = [[GOPManager alloc] initWithCustomID:customID verifyUrl:verify_Url timeout:5.0];
+	```
+
 2. 调用校验接口
-3. 处理结果
-4. 处理错误
+
+	```Objective-C
+	[self.manager verifyPhoneNum:@"输入的合法手机号" withCaptchaValidate:nil completion:^(NSDictionary *dict) {
+	        
+	    } failure:^(NSError *error) {
+	        
+	    }];
+	``` 
 
 >集成代码参考下方的**代码示例**
 
 ## 编译并运行你的工程
 
-编译你的工程, 体验全新的极验onepass产品！
+编译你的工程, 体验全新的极验 OnePass 产品！
 
 ![build](./img/build.png)
 
@@ -99,35 +108,25 @@ git clone git@github.com:GeeTeam/gop-ios-sdk.git
 	
 - (GOPManager *)manager {
     if (!_manager) {
-        _manager = [[GOPManager alloc] initWithCustomID:@"<---我应该为32位哟--->" configUrl:config_url verifyUrl:verify_url timeout:10.0];
+        _manager = [[GOPManager alloc] initWithCustomID:@"<---我应该为32位哟--->" configUrl:config_url verifyUrl:verify_url timeout:5.0];
     }
     
     return _manager;
 }
 ```
 
-### 进行人机验证
-
-```objc
-- (void)startCaptcha {
-	// 请参考demo或test-button内容
-}
-```
+### 进行 OnePass 校验
 	
-### 进行onepass校验
-	
-获取到人机验证的`validate`后, 自定义方法`verifyPhoneNum:`来进行对本机号码校验:
+使用 `OnePass.framework` 接口方法 **`verifyPhoneNum:withCaptchaValidate:completion:failure:`** 对本机号码校验
 	
 ```objc
-- (void)verifyPhoneNum:(NSString *)num validate:(NSString *) validate {
+- (void)verifyPhoneNum:(NSString *)num {
     
-    //自定义规则检测输入的手机号码的合法性
+    // 自定义规则检测输入手机号码的合法性
     if (![self checkPhoneNumFormat:num]) return;
     
-    // TODO UI相关操作
-    
-    // TODO 调用onepass校验接口
-	[self.manager verifyPhoneNum:num withCaptchaValidate:validate completion:^(NSDictionary *dict) {
+    // 调用 OnePass 校验接口
+	[self.manager verifyPhoneNum:num withCaptchaValidate:nil completion:^(NSDictionary *dict) {
         ...
     } failure:^(NSError *error) {
         ...
@@ -135,26 +134,26 @@ git clone git@github.com:GeeTeam/gop-ios-sdk.git
 }
 ```
 
+> Note: 在 `verifyPhoneNum:withCaptchaValidate:completion:failure:` 方法中，`validate` 参数默认传值 nil。极验提供身份验证(OnePass) 和 人机验证(Sensebot)，当开发者只需部署身份验证时，`validate` 参数值传入 nil 即可；当开发者需结合部署人机验证和身份验证时，`validate` 参数值从人机验证成功返回结果中获得。如需结合部署人机验证和身份验证，请联系极验后台修改相应配置。
+
 ## 处理校验结果以及错误
 
-onepass在校验成功后, 返回的onepass结果, 如果失败通过短信验证码作为补充
+OnePass 校验成功直接通过验证, 如果失败可以通过短信验证码作为补充。开发者可在 `OnePass.framework` 接口方法 **`verifyPhoneNum:withCaptchaValidate:completion:failure:`** 成功或者失败的回调中处理对应逻辑
 
 ```objc
-NSString *num = 手机号;
-NSString *validate = 验证校验结果
+NSString *num = @“校验后的合法手机号“;
 
-[self.manager verifyPhoneNum:num withCaptchaValidate:validate completion:^(NSDictionary *dict) {
+[self.manager verifyPhoneNum:num withCaptchaValidate:nil completion:^(NSDictionary *dict) {
     NSString *type = [dict objectForKey:@"type"];
-    if ([type isEqualToString:@"onepass"]) {// No sense Success
-        // TODO onepass成功
+    if ([type isEqualToString:@"onepass"]) { // No sense Success
+        // TODO OnePass 成功
     }
     else {
-        // TODO onepass失败, 使用短信验证作为补充
-        
+        // TODO OnePass 失败, 可使用短信验证作为补充
     }
 } failure:^(NSError *error) {
     [self.nextButton gtm_removeIndicator];
-    if (error.code != -999) {// 忽略-999
+    if (error.code != -999) { // 忽略-999
       // TODO 处理错误
     }
     NSLog(@"error: %@", error);
